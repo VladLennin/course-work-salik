a8f2bf257443eb81ce59680e8c71514a7bc57c67
Vladlen <88963754+VladLennin@users.noreply.github.com>
2023-11-01 12:26:20 +0200

    inital commit for template

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from "./component/Wrapper/Header/Header";
+import AppRouter from "./router/AppRouter";
+import Footer from "./component/Wrapper/Footer/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Header/>
+            <div className={"appContainer"}>
+                <AppRouter/>
+            </div>
+            <Footer/>
+        </>
+    );
 }
 
 export default App;
